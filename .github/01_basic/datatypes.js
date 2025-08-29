847612ea94b5fc53d37d936d9ea9d0ee5202afd8
mittalpulkit09 <mittalpulkit09@gmail.com>
2025-08-29 07:27:05 +0000

    change made in datatypes

--- .github/01_basic/datatypes.js
+++ .github/01_basic/datatypes.js
@@ -9,9 +9,8 @@ console.log(3
 console.log("Pulkit")
 
 
-let name = "hitesh"
+let name = "ansh"
 let age = 18
-let isLoggedIn = false
 let state;
 
 // number => 2 to power 53
@@ -27,3 +26,40 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heros);
+
